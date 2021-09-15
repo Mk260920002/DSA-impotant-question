2645664181afc778059dd4091e915c4dda39e280
Mk260920002 <87280466+Mk260920002@users.noreply.github.com>
2021-09-15 07:25:48 +0530

    Update MMaximumAreaOfRectangleInHistogram.cpp
    in this question first of all we find NSL(next smaller to left of an element) ansd NSR(next smaller to right) and then difference of NSR-NSL give width of that element now we mutiply that element with width *(NSR-NSL)

--- MMaximumAreaOfRectangleInHistogram.cpp
+++ MMaximumAreaOfRectangleInHistogram.cpp
@@ -33,9 +33,12 @@
    
       long long maxi=-2;
       for(int i=0;i<n;i++){
+        
           long long area=((NSR[i]-NSL[i]-1)*arr[i]);
           maxi=max(area,maxi);
       }
      return maxi;
     }
     
+//in this question first of all we find NSL(next smaller to left of an element) ansd NSR(next smaller to right) and then //
+//difference of NSR-NSL give width of that element now we mutiply that element with width *(NSR-NSL)//
